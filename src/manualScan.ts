19c74f96d2da5b8844f29790e302333eeed59d51
Martin Mende <martinmende@MacBook-Pro.fritz.box>
2020-07-16 19:54:01 +0200

    :fire: refactor: cleanup

--- src/manualScan.ts
+++
@@ -1,7 +0,0 @@
-import deviceDetection from './utils/detectDevices';
-
-const scan = async () => {
-  const devices = await deviceDetection();
-  console.log('Found devices', devices); // eslint-disable-line
-};
-scan();
