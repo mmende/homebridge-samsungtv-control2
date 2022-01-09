c24044bc64abaad22abc66b94fd738f75524f1d9
Philip Rader <pjrader1@yahoo.com>
2022-01-09 14:39:30 -0700

    Update `parseSerialNumber` for 2020 model numbers

--- src/utils/parseSerialNumber.ts
+++ src/utils/parseSerialNumber.ts
@@ -13,25 +13,19 @@ const markets = {
   A: `Asia`,
 } as const
 const years = {
-  Q: [2017, 2018, 2019],
-  RU: 2019,
-  NU: 2018,
-  N: 2018,
-  LS: 2018,
-  MU: 2017,
-  M: 2017,
-  KS: 2016,
-  KU: 2016,
-  K: 2016,
-  JS: 2015,
-  JU: 2015,
-  J: 2015,
-  HU: 2014,
-  H: 2014,
+  T: 2020, // TU: 2020
+  R: 2019, // RU: 2019
+  Q: [2017, 2018, 2019, 2020],
+  N: 2018, // NU: 2018
+  LS: [2015, 2017, 2018, 2019, 2020],
+  // For the LS** model numbers, they all start with "LS" and and with their year code,
+  // so getting a more accurate year could be possible.
+  M: 2017, // MU: 2017
+  K: 2016, // KS, KU: 2016
+  J: 2015, // JS, JU: 2015
+  H: 2014, // HU: 2014
   F: 2013,
-  E: 2012,
-  ES: 2012,
-  EH: 2012,
+  E: 2012, // ES, EH: 2012
   D: 2011,
   C: 2010,
   B: 2009,
@@ -52,6 +46,7 @@ export interface SamsungTVModel {
 /**
  * Parses Samsung TV Serial numbers into more specific infos
  * @see https://www.samsung.com/de/support/tv-audio-video/was-bedeutet-der-modellcode-meines-fernsehers/
+ * @see https://www.samsung.com/us/support/answer/ANS00087664/
  * @param sn SerialNumber
  */
 export default (sn: string): SamsungTVModel | null => {
